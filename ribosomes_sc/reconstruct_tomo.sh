17d749c03ebd275e3b3040ce129b09c28f265511
elons3 <152450688+elons3@users.noreply.github.com>
2024-07-25 15:53:16 -0600

    remove deliver command to see if thats the problem

--- ribosomes_sc/reconstruct_tomo.sh
+++ ribosomes_sc/reconstruct_tomo.sh
@@ -30,6 +30,5 @@ direct_file=/home/ejl62/template_matching/ribosomes_sc/test_batch.adoc
 batchruntomo -di "$direct_file" \
     -root "$root_name" \
     -current "$tomogram_path" \
-    -deliver "$output_path" \
-    -gpus 1 \
-    -cpus 2 
+    -gpus Linux:1 \
+    -cpus Linux:2 
