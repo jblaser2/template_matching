68768e833a79311840bedf9687c7f986759e8b3f
elons3 <152450688+elons3@users.noreply.github.com>
2024-07-25 11:44:43 -0600

    debugging path vars

--- ribosomes_sc/ribosome_tm_qd.sh
+++ ribosomes_sc/ribosome_tm_qd.sh
@@ -20,6 +20,8 @@
 
 # Set the max number of threads to use for programs using OpenMP. Should be <= ppn. Does nothing if the program doesn't use OpenMP.
 export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
+export PATH=/apps/spack/root/opt/spack/linux-rhel9-haswell/gcc-13.2.0/cuda-12.4.1-pw6cogp5nuczn2qcgqnw6lvqdznny2ef/bin:${PATH}
+export LD_LIBRARY_PATH=/apps/spack/root/opt/spack/linux-rhel9-haswell/gcc-13.2.0/cuda-12.4.1-pw6cogp5nuczn2qcgqnw6lvqdznny2ef/lib64:${LD_LIBRARY_PATH}
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE 
 module load spack
