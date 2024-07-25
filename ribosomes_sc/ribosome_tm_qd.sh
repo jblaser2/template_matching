cffb1f3b235b2c26d1840cdf53c6fe4f67da6ad8
elons3 <152450688+elons3@users.noreply.github.com>
2024-07-25 11:29:26 -0600

    Update ribosome_tm_qd.sh

--- ribosomes_sc/ribosome_tm_qd.sh
+++ ribosomes_sc/ribosome_tm_qd.sh
@@ -19,15 +19,12 @@
   
 
 # Set the max number of threads to use for programs using OpenMP. Should be <= ppn. Does nothing if the program doesn't use OpenMP.
-
 export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
-export PATH=/apps/spack/root/opt/spack/linux-rhel9-haswell/gcc-13.2.0/cuda-12.4.1-pw6cogp5nuczn2qcgqnw6lvqdznny2ef/bin:${PATH}
-export LD_LIBRARY_PATH=/apps/spack/root/opt/spack/linux-rhel9-haswell/gcc-13.2.0/cuda-12.4.1-pw6cogp5nuczn2qcgqnw6lvqdznny2ef/lib64:${LD_LIBRARY_PATH} 
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE 
-
-module load miniconda3/24.3.0-poykqmt
-module load cuda/12.4.1-pw6cogp
+module load spack
+module load miniconda3
+module load cuda
 
 source activate pytom
 
