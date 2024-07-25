c0433723b46b64280f12870d743c6154f14f3916
elons3 <152450688+elons3@users.noreply.github.com>
2024-07-25 15:26:27 -0600

    tomogram reconstruction debugging

--- ribosomes_sc/reconstruct_tomo.sh
+++ ribosomes_sc/reconstruct_tomo.sh
@@ -2,13 +2,17 @@
 
 #SBATCH --time=00:30:00 # walltime
 
-#SBATCH --ntasks=1 # number of processor cores (i.e. tasks)
+#SBATCH --ntasks=20 # number of processor cores (i.e. tasks)
 
 #SBATCH --nodes=1 # number of nodes
 
+#SBATCH --gpus=a100:1
+
+#SBATCH --qos=standby
+
 #SBATCH --export=NONE
 
-#SBATCH --mem 5G
+#SBATCH --mem 15G
 
 
 # Set the max number of threads to use for programs using OpenMP. Should be <= ppn. Does nothing if the program doesn't use OpenMP.
@@ -21,8 +25,6 @@ export LD_LIBRARY_PATH=/apps/spack/root/opt/spack/linux-rhel9-haswell/gcc-13.2.0
 tomogram_path="/home/ejl62/fsl_groups/grp_tomo_db1_d1/nobackup/archive/TomoDB1_d1/FlagellarMotor_P1/Hylemonella gracilis/yc2013-01-03-1"
 output_path="/home/ejl62/template_matching_shared/reconstruction_test"
 root_name="Bdel_Hyle_0010002.mrc"
+direct_file=/home/ejl62/template_matching/ribosomes_sc/test_batch.adoc 
 
-batchruntomo -root "$root_name" \
-    -current "$tomogram_path" \
-    -deliver "$output_path" \
-    /home/ejl62/template_matching/ribosomes_sc/test_batch.adoc 
+batchruntomo -di "$direct_file" -gpus 1 -cpus 20 
