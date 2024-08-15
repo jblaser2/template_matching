3287f6f192c56adb4e548a6236634d9510c3cca9
jblaser2 <144287726+jblaser2@users.noreply.github.com>
2024-08-15 10:37:50 -0600

    Update ribosome_tm_qd.sh

--- ribosomes_sc/ribosome_tm_qd.sh
+++ ribosomes_sc/ribosome_tm_qd.sh
@@ -1,32 +1,24 @@
 #!/bin/bash --login
-
 #SBATCH --time=0:20:00 # walltime
-
 #SBATCH --ntasks=2 # number of processor cores (i.e. tasks)
-
 #SBATCH --nodes=1 # number of nodes
-
 #SBATCH --gpus=v100:1
-
 #SBATCH --qos=msg
-
 #SBATCH --export=NONE
-
 #SBATCH --mem 10G
-
 #SBATCH --job-name=qdtm_45
-
   
-
 # Set the max number of threads to use for programs using OpenMP. Should be <= ppn. Does nothing if the program doesn't use OpenMP.
 export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
 export PATH=/apps/spack/root/opt/spack/linux-rhel9-haswell/gcc-13.2.0/cuda-12.4.1-pw6cogp5nuczn2qcgqnw6lvqdznny2ef/bin:${PATH}
 export LD_LIBRARY_PATH=/apps/spack/root/opt/spack/linux-rhel9-haswell/gcc-13.2.0/cuda-12.4.1-pw6cogp5nuczn2qcgqnw6lvqdznny2ef/lib64:${LD_LIBRARY_PATH}
-
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE 
 module load miniconda3
 module load cuda
 
+# set root directory to however you are accessing the shared template matching folder
+root_dir="/home/ejl62/eben_s/template_matching"
+# set hyperparameters
 angle=45
 
 source activate pytom
@@ -36,6 +28,10 @@ pytom_match_template.py \
  -m /home/ejl62/eben_s/template_matching/pytom/pytom_tutorial/templates/mask_60S.mrc \
  -v /home/ejl62/eben_s/template_matching/pytom/pytom_tutorial/dataset/tomo200528_100.mrc \
  -d "/home/ejl62/eben_s/template_matching/qd_tm_test_results/$angle" \
+ -t "$root_dir/pytom/pytom_tutorial/templates/60S.mrc" \
+ -m "$root_dir/pytom/pytom_tutorial/templates/mask_60S.mrc" \
+ -v "$root_dir/pytom/pytom_tutorial/dataset/tomo200528_100.mrc" \
+ -d "$root_dir/qd_tm_test_results/$angle" \
  --particle-diameter 300 \
  -a -$angle $angle \
  --low-pass 35 \
@@ -49,12 +45,15 @@ pytom_match_template.py \
 if [ $? -eq 0 ]; then
     pytom_estimate_roc.py \
     -j "/home/ejl62/eben_s/template_matching/qd_tm_test_results/$angle/tomo200528_100_job.json" \
+    -j "$root_dir/qd_tm_test_results/$angle/tomo200528_100_job.json" \
     -n 800 \
     -r 8 \
     --bins 16 \
     --crop-plot  > "//home/ejl62/eben_s/template_matching/qd_tm_test_results/$angle/tomo200528_100_roc.log"
+    --crop-plot  > "/$root_dir/qd_tm_test_results/$angle/tomo200528_100_roc.log"
 
     pytom_extract_candidates.py -j "/home/ejl62/eben_s/template_matching/qd_tm_test_results/$angle/tomo200528_100_job.json" \
+    pytom_extract_candidates.py -j "$root_dir/qd_tm_test_results/$angle/tomo200528_100_job.json" \
     -n 300 \
     -r 8 \
     -c 0.4
